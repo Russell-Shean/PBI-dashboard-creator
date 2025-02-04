69f0783b108e6c25c358fc3c3990688ac299f3d2
Russell Shean <73149125+Russell-Shean@users.noreply.github.com>
2025-02-04 15:06:32 -0800

    update import in create_example_dashboard.py

--- examples/create_example_dashboard.py
+++ examples/create_example_dashboard.py
@@ -1,15 +1,4 @@
-import PBI_dashboard_creator.create_blank_dashboard as PBI_dash
-import PBI_dashboard_creator.add_local_csv as PBI_local_csv
-import PBI_dashboard_creator.add_tmdl as PBI_tmdl
-
-import PBI_dashboard_creator.create_date_hrcy as PBI_date_hr
-import PBI_dashboard_creator.create_new_page as PBI_pages
-import PBI_dashboard_creator.create_new_chart as PBI_charts
-import PBI_dashboard_creator.add_background_image as PBI_bg_img
-import PBI_dashboard_creator.add_ADLS_csv as PBI_blob_csv
-import PBI_dashboard_creator.add_text_box as PBI_text_box
-import PBI_dashboard_creator.add_button as PBI_button
-import PBI_dashboard_creator.add_shape_map as PBI_map
+import PBI_dashboard_creator as PBI
 
 import os
 
@@ -27,16 +16,16 @@ PBI_dash.create_new_dashboard(report_location, report_name)
 
 # add data -------------------------------------------------------------------------------------------------------
 # add locally stored csv files to the new dashboard
-PBI_local_csv.add_csv(dashboard_path, os.path.join(report_location, "PBI_dashboard_creator/examples/data/colony.csv" ))
-PBI_local_csv.add_csv(dashboard_path, os.path.join(report_location, "PBI_dashboard_creator/examples/data/wa_bigfoot_by_county.csv" ))
+PBI.add_csv(dashboard_path, os.path.join(report_location, "PBI_dashboard_creator/examples/data/colony.csv" ))
+PBI.add_csv(dashboard_path, os.path.join(report_location, "PBI_dashboard_creator/examples/data/wa_bigfoot_by_county.csv" ))
 
 # add the default DateTable to the dashboard 
-PBI_tmdl.add_tmdl_dataset(dashboard_path = dashboard_path, data_path = None, add_default_datetable = True)
+PBI.add_tmdl_dataset(dashboard_path = dashboard_path, data_path = None, add_default_datetable = True)
 
 # add a csv file stored in ADLS
 
 try:
-	PBI_blob_csv.add_csv_from_blob(dashboard_path, 
+	PBI.add_csv_from_blob(dashboard_path, 
 	                           account_url = "https://sadohpowerbi.blob.core.windows.net",  
 	                           blob_name = "test",
 	                           data_path = "wa_wolfs.csv",
@@ -51,21 +40,21 @@ except:
 
 ## page 2 ---------------------------------------------------------------------------------------------------------
 # create a new page
-PBI_pages.add_new_page(dashboard_path, 
+PBI.add_new_page(dashboard_path, 
 	                   page_name = "Bee Colonies",
 	                   title= "The bees are in Trouble!",
 	                   subtitle = "We're losing bee colonies"
 	)
 
 # add background image
-PBI_bg_img.add_background_img(dashboard_path = dashboard_path, 
+PBI.add_background_img(dashboard_path = dashboard_path, 
 	               page_id = "page2", 
 	               img_path = "./PBI_dashboard_creator/examples/data/Taipei_skyline_at_sunset_20150607.jpg", 
 	               alpha = 51,
 	               scaling_method = "Fit")
 
 ## page 3 ------------------------------------------------------------------------------------------------------
-PBI_pages.add_new_page(dashboard_path, 
+PBI.add_new_page(dashboard_path, 
 	                   page_name = "Bigfoot Map",
 	                   title= "Bigfoot sightings",
 	                   subtitle = "By Washington Counties"
@@ -78,7 +67,7 @@ PBI_pages.add_new_page(dashboard_path,
 # Add visual elements ---------------------------------------------------------------------------------------------------
 
 # add a new column chart on the second page
-PBI_charts.add_chart(dashboard_path = dashboard_path, 
+PBI.add_chart(dashboard_path = dashboard_path, 
 	      page_id = "page2", 
 	      chart_id = "colonies_lost_by_year", 
 	      chart_type = "columnChart",
@@ -95,7 +84,7 @@ PBI_charts.add_chart(dashboard_path = dashboard_path,
 	      width = 603)
 
 # add a text box to the second page
-PBI_text_box.add_text_box(text = "Explanatory text in the bottom right corner",
+PBI.add_text_box(text = "Explanatory text in the bottom right corner",
              dashboard_path= dashboard_path,
                page_id = "page2",
                  text_box_id = "page2_explain_box", 
@@ -108,7 +97,7 @@ PBI_text_box.add_text_box(text = "Explanatory text in the bottom right corner",
 # add buttons
 
 # download data button (a link to an internet address)
-PBI_button.add_button(label = "Download Data",
+PBI.add_button(label = "Download Data",
   dashboard_path = dashboard_path,
   page_id = "page2",
   button_id = "page2_download_button",
@@ -119,7 +108,7 @@ PBI_button.add_button(label = "Download Data",
   url_link = "https://doh.wa.gov/data-and-statistical-reports/washington-tracking-network-wtn/opioids/overdose-dashboard#downloads")
 
 # navigate back to page 1 button
-PBI_button.add_button(label = "Back to page 1",
+PBI.add_button(label = "Back to page 1",
   dashboard_path = dashboard_path,
   page_id = "page2",
   button_id = "page2_back_to_page1_button",
@@ -132,7 +121,7 @@ PBI_button.add_button(label = "Back to page 1",
 
 ## Add a map to page 3 ----------------------------------------------------------------------
 
-PBI_map.add_shape_map(dashboard_path = dashboard_path, 
+PBI.add_shape_map(dashboard_path = dashboard_path, 
               page_id = "page3",
               map_id = "bigfoots_by_county_map",
               data_source = "wa_bigfoot_by_county",
